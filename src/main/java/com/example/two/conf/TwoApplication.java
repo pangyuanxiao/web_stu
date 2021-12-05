3fdb008ad09b5e7b9508c11da204320238f65e0c
xiadingxi <1079044182@qq.com>
2021-12-06 00:11:55 +0800

    2-6 hello接口 地址：http://127.0.0.1:8880/hello

--- src/main/java/com/example/two/conf/TwoApplication.java
+++ src/main/java/com/example/two/conf/TwoApplication.java
@@ -1,11 +1,20 @@
-package com.example.two;
+package com.example.two.conf;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+
+/*
+    ComponentScan只会扫描他所在目录的子包
+    他是支持扫多包的
+    @ComponentScan({"com.example.two"})
+ */
+
+@ComponentScan("com.example.two")
 @SpringBootApplication
 public class TwoApplication {
     private static final Logger LOG = LoggerFactory.getLogger(TwoApplication.class);
---
+++ src/main/java/com/example/two/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.example.two.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+/*
+     @RestController  一般用来返回字符串
+     @Controller 返回页面
+
+     RestController相当于Controller+ResponseBody
+     ResponseBody一般用来返回字符串或JSON对象（更多）
+ */
+
+
+
+
+@RestController
+public class TestController {
+    /*
+         get post put delete
+         RequestMapping支持所有的
+         如果向指定方法可以
+         @GetMapping
+         @PostMapping
+         @PutMapping
+         @DeleteMapping
+         或者
+         @RequestMapping(value = "/",method = RequestMethod.GET)
+
+     */
+
+    //@RequestMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        return "hello";
+    }
+}
