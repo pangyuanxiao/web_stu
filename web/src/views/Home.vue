eddf88cd30b5d9bc1b82fc68b2d6b5f84567f293
xiadingxi <1079044182@qq.com>
2022-04-24 02:15:52 +0800

    4-5  4-6 集成 ant-design-vue 以及网站首页

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,6 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+
+
+      <a-button type="primary">Primary Button</a-button>
+      <a-button>Default Button</a-button>
+      <a-button type="dashed">Dashed Button</a-button>
+      <a-button type="text">Text Button</a-button>
+      <a-button type="link">Link Button</a-button>
+
+
+
+
   </div>
 </template>
 
