45834b8afbe347fde47249dadd10979feada754a
xiadingxi <1079044182@qq.com>
2022-06-07 20:20:22 +0800

    6-4 打开sql日志，后端集成pagehelper

--- src/main/java/com/example/two/service/EbookService.java
+++ src/main/java/com/example/two/service/EbookService.java
@@ -7,6 +7,10 @@ import com.example.two.mapper.EbookMapper;
 import com.example.two.req.EbookReq;
 import com.example.two.resp.EbookResp;
 import com.example.two.utils.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -16,6 +20,7 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG =LoggerFactory.getLogger(EbookService.class);
 
     @Resource
 
@@ -28,10 +33,15 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(rep.getName())) {
             criteria.andNameLike("%" + rep.getName() + "%");
         }
+        //从第一页开始，每页有三个  要注意一下这个只对最近的sql有效
+        PageHelper.startPage(1, 3);
         //这个selectByExample相当于where语句
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         //循环ebookList里的实体，每个都变成 EbookResp 变成 EbookResp list
 
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 //            EbookResp ebookResp = new EbookResp();
