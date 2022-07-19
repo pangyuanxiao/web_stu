3fe35616a965e2234f7b33529aa6efbedd20e0f9
xiadingxi <1079044182@qq.com>
2022-07-19 23:12:24 +0800

    6-9 将后端的long类型转化成string传给前端

---
+++ src/main/java/com/example/two/conf/JacksonConfig.java
@@ -0,0 +1,24 @@
+package com.example.two.conf;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * 统一注解，解决雪花算法前后端交互Long类型精度丢失的问题
+ *
+ */
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        return objectMapper;
+    }
+}
